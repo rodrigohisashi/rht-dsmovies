b61811ac48b8893633d268b1a2ae4a4805bedc9b
Rodrigo Hisashi <rodrigohisa@gmail.com>
2022-01-16 19:38:33 -0300

    Ligando Front e Backend

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,16 +1,50 @@
 import Pagination from "components/Pagination";
 import MovieCard from "components/MovieCard";
+import {BASE_URL} from "utils/requests"
+import axios from "axios"
+import {useState, useEffect} from "react"
+import {MoviePage} from "types/movie"
 
 function Listing() {
+
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
+        
+    });
+
+    const [pageNumber, setPageNumber] = useState(0);
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`).then(response => {
+            const data = response.data as MoviePage;
+            setPage(data);
+
+        });
+        }, [pageNumber] );
+
+
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
+    
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange}/>
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-6 col-lg-4 col-xl-3">
+                    {page.content.map(item => ( 
+                        <div key={item.id} className="col-sm-6 col-lg-4 col-xl-3">
 
-                        <MovieCard />
-                    </div>
+                        <MovieCard movie={item} />
+                    </div>))}
+        
                 </div>
             </div>
         </>
