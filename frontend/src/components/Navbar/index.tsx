9f3a16a6948df54ff2edff054ff14d37032329ca
Rodrigo Hisashi <rodrigohisa@gmail.com>
2022-01-16 19:56:36 -0300

    Corrigido Links Externos

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -6,8 +6,8 @@ function Navbar() {
     <header>
         <nav className="container">
             <div className="dsmovie-nav-content">
-                <h1>RHT - Movies</h1>
-                <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
+                <a href="/"> <h1>RHT - Movies</h1> </a>
+                <a href="https://github.com/rodrigohisashi" target="_blank" rel="noreferrer">
                     <div className="dsmovie-contact-container">
                         <GithubIcon />
                         <p className="dsmovie-contact-link">/rodrigohisashi</p>
